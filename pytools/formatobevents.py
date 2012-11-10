2d640dee053584a31f2a11ef2a92d6b48e591c9a
jule64@gmail.com <jule64@gmail.com>
2012-11-10 02:30:26 +0000

    improved pytool data formatting script which includes separated headers and values rows and automatic split of data into separate files per instruments

--- pytools/formatobevents.py
+++ pytools/formatobevents.py
@@ -1,63 +1,209 @@
 '''
-Formats the data in obevents.txt for use with spreadsheets 
-and data analysis tools and saves the result in /data/fileout.txt
+Formats data contained in obevents.txt for use with spreadsheets 
+and data analysis tools and saves the result in separate
+files (one file per instrument) in data/
 
 
 
 TODO 
-    1/ auto rename fileout.txt with name provided as second (optional) argument
-in script and add date time at the beginning of the file name
-    2/ delete data in obevents after file has been succesfully output to outfile.txt
-    (ensure it does not conflict with potential parallel processes that try to write to
-    obevents (like LmaxTrading.java)
+    1/ delete data in obevents.txt once process completes (ensure first that
+    no conflict arise potentially running parallel processes like LmaxTrading.java's
+    own file writing process on obevents.txt during live sessions
 
 
 @author: jule64@gmail.com
 '''
 import re
+import string
+import datetime
+import time
+import os
+from symbol import if_stmt
+
+
+class processdata():
+    
+ 
+    def extractheadersandvalues(self):
+        
+
+    
+    # run below only for the first line in the file
+        if self.headerflag:
+            
+            # extract headers
+            linesp = re.findall(r'(\b[\w-]+)=', self.line)
+        
+            # merge headers into one string
+            finstrg = ''
+            for strg in linesp:
+                finstrg += strg+','
+            
+            # remove crappy comma at the end
+            self.line = finstrg[:len(finstrg)-1]
+            
+            self.headerflag = False
+            return
+            
+    
+    
+    # run below for ALL the lines in the file
+    
+        # extract values
+        patfind = re.compile(r'=([\w\d\.\/]+)\b')
+        linesp = re.findall(patfind, self.line)
+    
+        # merge values into one string
+        finstrg = ''
+        for strg in linesp:
+            finstrg += strg+','
+        # remove crappy comma at the end
+        self.line = finstrg[:len(finstrg)-1]
 
 
 
+
+
+            
+    def keepheadersandvaluesonly(self):
+                # remove crap at beginning and end
+                self.line = re.sub(r'OrderBookEventImpl{', '', self.line)
+                self.line = re.sub(r'}, timereceived', ', timereceived', self.line)
+                
+                # remove single quotes
+                self.line = re.sub(r'\'', '', self.line)
+                
+                
+                # extract first part of string
+                part1 = re.findall(r'.*bidPrices', self.line)[0]
+                part1 = re.sub('bidPrices', '', part1)
+                
+                # extract second part of string        
+                part2 = re.findall(r'bidPrices.*\], askPrices', self.line)[0]
+                part2 = re.sub('askPrices', '', part2)
+                part2 = re.sub('bidPrices=\[', '', part2)        
+                part2 = re.sub('PricePoint{', '', part2)        
+                part2 = re.sub('price', 'bid-price', part2)
+                part2 = re.sub('quantity', 'bid-quantity', part2)
+                part2 = re.sub('}', '', part2)
+                part2 = re.sub('\]', '', part2)
+                
+                # extract third part of string
+                part3 = re.findall(r'askPrices.*', self.line)[0]
+                part3 = re.sub('askPrices=\[', '', part3)        
+                part3 = re.sub('PricePoint{', '', part3)        
+                part3 = re.sub('price', 'ask-price', part3)
+                part3 = re.sub('quantity', 'ask-quantity', part3)
+                part3 = re.sub('}', '', part3)
+                part3 = re.sub('\]', '', part3)        
+                
+                # merge all sub strings together
+                self.line = part1 + part2 + part3
+                
+#                print self.line
+#            
+#            #write result to file
+#            instrufile.write(line)
+
+
+
+
+    def processfile(self):
+        instruidlist = []
+        filelist = []
+        
+        
+
+        mainfile = open("/Users/julienmonnier/workspace/LMAXTrading/obevents.txt", "r")
+        self.line = mainfile.readline()
+
+        # main loop.  We are reading the content of mainfile line by line so as
+        # to reduce memory footprint and increase reading speed
+        while self.line:
+            
+            #store content of file in variable
+            #TODO check that there is no size limit for the variable
+            
+            idpat = re.compile(r'\d+')
+
+            # Extract security ID of the security
+            instruid = re.search(idpat, self.line).group()
+            # check if instru ID already in list and if
+            # not add it and create a new file in data
+            # folder with name of instruid
+            if instruid not in instruidlist:
+                instruidlist.append(instruid)
+                # create file (if doesn't exist from previous runs
+                # the w+ ensures any data from previous runs is overwritten 
+                instrufile = open('/Users/julienmonnier/workspace/LMAXTrading/data/'+instruid+'.txt', 'w+')
+                # add newly open file to a list for data processing in next step
+                # note we add a key value for faster retrieval during data processing
+                # steps
+                filelist.append([instruid,instrufile])
+                self.headerflag = True
+
+            
+            
+            self.keepheadersandvaluesonly()
+            self.extractheadersandvalues()
+            
+            
+            
+            
+            
+            
+            print self.line
+            
+            
+            
+            
+                      
+            # write the current line to the relevant file based on the instrument id
+            for instrufile in filelist:
+                if instrufile[0] == (instruid):
+                    instrufile[1].write(self.line+'\n')
+            
+            # load next line in main file. if no more lines the while
+            # loop exits
+            self.line = mainfile.readline()
+            
+    
+        # last step:
+        # closing all open files
+        for instrufile in filelist:
+            instrufile[1].close()
+            
+        mainfile.close()
+        
+        
+        
 
 
 if __name__ == '__main__':
-    with open("/Users/julienmonnier/workspace/LMAXTrading/obevents.txt", "r") as sources:
-        lines = sources.readlines()
+    processdata().processfile()
+
+
+
+#    line = '''instrumentId=4001, valuationBidPrice=1.26981, timeStamp=1352467491695, valuationAskPrice=1.26984, lastMarketClosePrice=1.27466, bid-price=1.26981, bid-quantity=275, bid-price=1.2698, bid-quantity=450, bid-price=1.26979, bid-quantity=1200, bid-price=1.26978, bid-quantity=375, bid-price=1.26976, bid-quantity=250, ask-price=1.26984, ask-quantity=325, ask-price=1.26985, ask-quantity=250, ask-price=1.26986, ask-quantity=1250, ask-price=1.26987, ask-quantity=100, ask-price=1.26988, ask-quantity=775, lastTradedPrice=1.26982, dailyHighestTradedPrice=1.27906, dailyLowestTradedPrice=1.269, marketClosePriceTimeStamp='1352412000000', timereceived='1352467492255', instruname=EUR/USD, obeventrow=1'''
+#    print re.findall(r'(\b[\w-]+)=', line)
+
     
-    with open("/Users/julienmonnier/workspace/LMAXTrading/data/fileout.txt", "w") as sources:
-        for line in lines:
-            # remove crap at beginning and end
-            line = re.sub(r'OrderBookEventImpl{', '', line)
-            line = re.sub(r'}, timereceived', ', timereceived', line)
-            
-            
-            # extract first part of string
-            part1 = re.findall(r'.*bidPrices', line)[0]
-            part1 = re.sub('bidPrices', '', part1)
-            
-            # extract second part of string        
-            part2 = re.findall(r'bidPrices.*\], askPrices', line)[0]
-            part2 = re.sub('askPrices', '', part2)
-            part2 = re.sub('bidPrices=\[', '', part2)        
-            part2 = re.sub('PricePoint{', '', part2)        
-            part2 = re.sub('price', 'bid-price', part2)
-            part2 = re.sub('quantity', 'bid-quantity', part2)
-            part2 = re.sub('}', '', part2)
-            part2 = re.sub('\]', '', part2)
-            
-            # extract third part of string
-            part3 = re.findall(r'askPrices.*', line)[0]
-            part3 = re.sub('askPrices=\[', '', part3)        
-            part3 = re.sub('PricePoint{', '', part3)        
-            part3 = re.sub('price', 'ask-price', part3)
-            part3 = re.sub('quantity', 'ask-quantity', part3)
-            part3 = re.sub('}', '', part3)
-            part3 = re.sub('\]', '', part3)        
-            
-            # merge all sub strings together
-            line = part1 + part2 + part3 +'\n'
-            
-            #write result to file
-            sources.write(line)
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
