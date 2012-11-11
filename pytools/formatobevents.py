e44c38d8b811c86ee3ddc6e45812504c7434ccac
jule64@gmail.com <jule64@gmail.com>
2012-11-11 01:35:39 +0000

    pytools/formatobevents.py: reworked overall process flow, renamed some methods with more self documenting names and added performance tracking stats.

--- pytools/formatobevents.py
+++ pytools/formatobevents.py
@@ -1,8 +1,8 @@
-'''
-Formats data contained in obevents.txt for use with spreadsheets 
-and data analysis tools and saves the result in separate
-files (one file per instrument)
 
+
+"""Script for formating data contained in obevents.txt into a 
+spreadsheet-friendly format and saves the result in separate
+files (one file per instrument)
 
 The data in obevents.txt has the following format:
 
@@ -25,115 +25,139 @@ The formatted data looks like this:
 #    ,250,1.26986,1250,1.26987,100,1.26988,775,1.26982,1.27906,1.269,1352412000000,1352467492255,EUR/USD,1
 
 
-The formatted data is stored in: /data/
+The formatted data is stored in: /data/'instrumentid'.txt
 
-
-
-TODO 
-    1/ delete data in obevents.txt once process completes (ensure first that
-    no conflict arise potentially running parallel processes like LmaxTrading.java's
-    own file writing process on obevents.txt during live sessions
+NOTE on performance:  current performance tuning tests indicate an
+average process time of 0.3 milliseconds (0.0003s) per line of data.
+This performance implies an expected process time of about 2 minutes
+for a file containing 360,000 rows of data (which corresponds to the average
+number of tick data received during the course of a normal trading day using
+current standard LMAX API access)
 
 
 @author: jule64@gmail.com
-'''
+"""
+
+
+#TODO 
+#    1/ delete data in obevents.txt once process completes (ensure first that
+#    no conflict arise potentially running parallel processes like LmaxTrading.java's
+#    own file writing process on obevents.txt during live sessions
+
+
+
 import re
-import string
-import datetime
 import time
-import os
 
 
-class processdata():
-    
-    
-    
-    def processfile(self):
-        instruidlist = []
-        self.filelist = []
+class ProcessData():
 
-        mainfile = open("/Users/julienmonnier/workspace/LMAXTrading/obevents.txt", "r")
-        self.line = mainfile.readline()
+    """Contains static methods for formatting the obevents.txt data
+    Currently the class contains one public method, processfile().
+    The other methods in this class have been prefixed with an '_'
+    to signal that they should not be called by the client code
+    (although they can as any good Python programmer knows)
+    """
+    
+    # main method
+    @classmethod
+    def processfile(cls, timeproc=False):
+        cls.instruidlist = []
+        cls.filelist = []
         
-        # main loop.  We are reading the content of mainfile line by line so as
-        # to reduce memory footprint and increase reading speed
-        while self.line:
-            
-            print self.line
-            #store content of file in variable
-            #TODO check that there is no size limit for the variable
-            
-            idpat = re.compile(r'\d+')
+        cls.timeproc = timeproc
+        cls.timeelepsed = []
 
-            # Extract security ID of the security
-            self.instruid = re.search(idpat, self.line).group()
-            # check if instru ID already in list and if
+        # could be a with statement
+        with open("/Users/julienmonnier/workspace/LMAXTrading/obevents.txt", "r") as mainfile:
+            
+            
+            
+            
+            
+            cls.line = mainfile.readline()
+            
+            # main loop.  We are reading the content of mainfile line by line and process
+            # formatting statements on it before writing the final result to the relevant
+            # instrument file
+            # NOTE: using a while statement (as opposed to a readlines()) allows to reduce
+            # memory footprint and speed up the overall process
+            while cls.line:
+                
+                # save current time for performance monitoring
+                cls._starttime = time.time()
+                
+                cls._open_instrument_file()            
+                cls._keep_headervalue_pairs_only()
+                if cls.headerflag:
+                    cls._extractheaders()
+                    cls._writetofile()
+                cls._extractvalues()
+                cls._writetofile()
+                cls.line = mainfile.readline()
+                
+                # save current time for performance monitoring
+                cls._endtime = time.time()
+                
+                # add elapsed time to list for future use
+                cls.timeelepsed.append(cls._endtime - cls._starttime)
+            
+            # close all open files
+            for instrufile in cls.filelist:
+                instrufile[1].close()
+            
+            # display time statistics if requested
+            if cls.timeproc:
+                print sum(cls.timeelepsed) / len(cls.timeelepsed)
+                
+    # =================================================
+    # =================================================
+
+    # opens a new instrument file based on value of instrument ID retrieved from
+    # cls.line.  Does nothing if file already open
+    @classmethod
+    def _open_instrument_file(cls):
+        # Extract security ID from cls.line and save to class instance for later use
+            cls.instruid = re.search(r'\d+', cls.line).group()
+            # check if security ID is already in list and if
             # not add it and create a new file in data
-            # folder with name of self.instruid
-            if self.instruid not in instruidlist:
-                instruidlist.append(self.instruid)
+            # folder with name of cls.instruid
+            if cls.instruid not in cls.instruidlist:
+                cls.instruidlist.append(cls.instruid)
                 # create file (if doesn't exist from previous runs
                 # the w+ ensures any data from previous runs is overwritten 
-                instrufile = open('/Users/julienmonnier/workspace/LMAXTrading/data/'+self.instruid+'.txt', 'w+')
+                instrufile = open('/Users/julienmonnier/workspace/LMAXTrading/data/'+cls.instruid+'.txt', 'w+')
                 # add newly open file to a list for data processing in next step
                 # note we add a key value for faster retrieval during data processing
                 # steps
-                self.filelist.append([self.instruid,instrufile])
-                self.headerflag = True
-
-            
-            
-            self.keep_headervalue_pairs_only()
-            
-            if self.headerflag:
-                self.extractheaders()
-                self.writetofile()
-                
-            self.extractvalues()
-            self.writetofile()
-
-            
-
-            # last statement in while loop
-            # loads next line in main file into line 
-            # if no more lines, while loop exits
-            self.line = mainfile.readline()
-        
-        # first statement after while loop
-        # close all open files
-        for instrufile in self.filelist:
-            instrufile[1].close()
-        mainfile.close()
-
-    # end of processdata()   
-    # =================================================
-    # =================================================
-
-    
+                cls.filelist.append([cls.instruid,instrufile])
+                cls.headerflag = True
     
     # does what it says on the tin
-    def keep_headervalue_pairs_only(self):
+    @classmethod
+    def _keep_headervalue_pairs_only(cls):
         
         # regexp to split line's data into named groups 
         p = re.compile(r'(?P<start>^.*)(?P<bid>[[][^]]*[]])(?P<middle>.*)(?P<ask>[[][^]]*[]])(?P<end>.*$)')
         
         # add bid prefix to bid prices and quantities and do the same for ask prices and quantities
-        self.line = (p.search(self.line).group('start')
-            + re.sub(r'quantity','bid-quantity',re.sub(r'price','bid-price',p.search(self.line).group('bid')))
-            + p.search(self.line).group('middle')
-            + re.sub(r'quantity','ask-quantity',re.sub(r'price','ask-price',p.search(self.line).group('ask')))
-            + p.search(self.line).group('end'))
+        cls.line = (p.search(cls.line).group('start')
+            + re.sub(r'quantity','bid-quantity',re.sub(r'price','bid-price',p.search(cls.line).group('bid')))
+            + p.search(cls.line).group('middle')
+            + re.sub(r'quantity','ask-quantity',re.sub(r'price','ask-price',p.search(cls.line).group('ask')))
+            + p.search(cls.line).group('end'))
         
         # remove all non necessary info contained in line in order to keep only pairs of 'header=value'
-        self.line = re.sub(r'OrderBookEventImpl|bidPrices=|askPrices=|PricePoint|[[\]{}\']','',self.line)
+        cls.line = re.sub(r'OrderBookEventImpl|bidPrices=|askPrices=|PricePoint|[[\]{}\']','',cls.line)
 
 
 
     # extracts headers
-    def extractheaders(self):
+    @classmethod
+    def _extractheaders(cls):
         
         # extract headers
-        linesp = re.findall(r'(\b[\w-]+)=', self.line)
+        linesp = re.findall(r'(\b[\w-]+)=', cls.line)
     
         # merge headers into one string
         finstrg = ''
@@ -141,40 +165,38 @@ class processdata():
             finstrg += strg+','
         
         # remove comma at the end and save to variable
-        self.linetowrite = finstrg[:len(finstrg)-1]
+        cls.linetowrite = finstrg[:len(finstrg)-1]
 
-        self.headerflag = False
+        cls.headerflag = False
         
         
     # extracts values
-    def extractvalues(self):
+    @classmethod
+    def _extractvalues(cls):
         
         # extract values
         patfind = re.compile(r'=([\w\d\.\/]+)\b')
-        linesp = re.findall(patfind, self.line)
+        linesp = re.findall(patfind, cls.line)
     
         # merge values into one string
         finstrg = ''
         for strg in linesp:
             finstrg += strg+','
         # remove comma at the end and save to variable
-        self.linetowrite = finstrg[:len(finstrg)-1]
+        cls.linetowrite = finstrg[:len(finstrg)-1]
 
 
     # write formatted line of data into the relevant file based on value of instrument id
-    def writetofile(self):
-        for instrufile in self.filelist:
-            if instrufile[0] == (self.instruid):
-                instrufile[1].write(self.linetowrite+'\n')
+    @classmethod
+    def _writetofile(cls):
+        for instrufile in cls.filelist:
+            if instrufile[0] == (cls.instruid):
+                instrufile[1].write(cls.linetowrite+'\n')
 
-
-    
-    
-        
 
 
 if __name__ == '__main__':
-    processdata().processfile()
+    ProcessData.processfile(True)
 
             
 
